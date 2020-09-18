381bff644eae32502b5474c2c09e06dfef0e3e7d
Harry <harry@codingbyharry.com>
2020-09-18 12:50:47 +1000

    implement jquery and select2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :fiesta_items, only: %i[index]
+
   namespace :manage do
     root to: 'dashboard#index'
     resources :items, only: [:new, :create]
