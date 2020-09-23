acd88d652886189b5c416bf6223ba1b08ed8e0ae
Harry <harry@codingbyharry.com>
2020-09-23 17:47:29 +1000

    fix merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,14 @@
 class User < ApplicationRecord
-  has_many :items
+  has_many :items, dependent: :restrict_with_error  
   has_many :characters
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
 
-<<<<<<< HEAD
   enum discord_status: {
     "unverified": 0,
     "pending": 1,
     "verified": 2,
   }
-=======
-  has_many :items, dependent: :restrict_with_error  
->>>>>>> 62713dd0deba55f1a54528ca4622a57e94fe48bb
 end
